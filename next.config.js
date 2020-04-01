285785805adf64dc2cf80392329772c0bd6814c9
8zca <5304269+8zca@users.noreply.github.com>
2020-04-02 00:18:06 +0900

    絶対パスでimportする (@/path/to/file) (#5)
    * Update import absolute path

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+module.exports = {
+  webpack: config => {
+    config.resolve.alias['@'] = path.resolve(__dirname) + "/src";
+    return config;
+  }
+};
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { sampleUserData } from '../../../utils/sample-data'
-
+// import { sampleUserData } from '../../../utils/sample-data'
+// import { sampleUserData } from '@utils/sample-data'
+import { sampleUserData } from '@/utils/sample-data'
 export default (_: NextApiRequest, res: NextApiResponse) => {
   try {
     if (!Array.isArray(sampleUserData)) {
