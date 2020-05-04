49a9a6d8da45fea863c15fa2e7721dfb66513c4d
8zca <excref@gmail.com>
2020-05-04 20:46:44 +0900

    user/[id] を SSR にした

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -1,10 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { sampleUserData } from '@/utils/sample-data'
 
-export default (_: NextApiRequest, res: NextApiResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const id = req.query.id as string
   try {
     if (!Array.isArray(sampleUserData)) {
       throw new Error('Cannot find user data')
+    }
+
+    const user = sampleUserData.find(row => row.id === Number(id))
+    if (user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).json({ statusCode: 404, message: 'Cannot find user' })
     }
 
     res.status(200).json(sampleUserData)
