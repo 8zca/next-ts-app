6de6bbbe69a791e53e2eb0162e8816a2511747ac
8zca <5304269+8zca@users.noreply.github.com>
2020-05-08 18:06:08 +0900

    Add theme (#13)
    * Add theme
    
    * Remove unnuse package

---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+export default {
+  colors: {
+    primary: {
+      light: '#7986cb',
+      dark: '#303f9f'
+    },
+    secondary: {
+      light: '#c51162',
+      dark: '#ff4081'
+    },
+    text: {
+      primary: 'rgba(0, 0, 0, 0.87)',
+      secondary: 'rgba(0, 0, 0, 0.54)',
+      terciary: 'rgba(0, 0, 0, 0.38)',
+      disabled: 'rgba(0, 0, 0, 0.38)'
+    },
+    action: {
+      hover: 'rgba(0, 0, 0, 0, 0.04)',
+      disabled: 'rgba(0, 0, 0, 0.26)'
+    }
+  },
+  fonts: {
+    body: 'system-ui, sans-serif'
+  }
+}
